18624ad5e856626ea4d9b223dffaeb6ffca8c64b
Parveen Bhadoo <87309724+PBhadoo@users.noreply.github.com>
2024-07-17 07:25:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Install git
+if ! command -v git &> /dev/null
+then
+    echo "git could not be found, installing..."
+    apt-get update -y
+    apt-get install git -y
+fi
+
+# Clone the repository
 git clone "https://github.com/temp-deployers/HHStreamENVBot" tgfs
 cd tgfs
+
+# Run the Python module
 python -m WebStreamer
